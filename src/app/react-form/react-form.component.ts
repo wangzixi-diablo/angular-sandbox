fffa0b01eafd1fb283d9aa47c49d072228d34029
wangzixi-diablo <cle.ee@163.com>
2021-01-18 15:28:17 +0800

    live change scenario works

--- src/app/react-form/react-form.component.ts
+++ src/app/react-form/react-form.component.ts
@@ -3,18 +3,22 @@ import { FormControl } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const ENDPOINT = "http://localhost:3000/echo?data=";
+const APIENDPOINT = "http://localhost:3000/echo?data=";
 
 @Component({
   selector: 'jerryform',
   template: `
     <input type="text" [formControl]="favoriteColorControl">
+    <div>{{ response }}</div>
   `
 })
 export class ReactFormComponent implements OnInit  {
 
   constructor(private http:HttpClient){
   }
+
+  response = '';
+
   ngOnInit(): void {
 
     const headers: HttpHeaders = new HttpHeaders({
@@ -28,10 +32,13 @@ export class ReactFormComponent implements OnInit  {
     this.favoriteColorControl.valueChanges.subscribe(
       (value) =>{
         console.log('new value from live change: ' + value);
-        var $http = this.http.get('http://localhost:3000/angular/' + value, options);
+        const url = `${APIENDPOINT}${value}` 
+        var $http = this.http.get(url, options);
         
         $http.subscribe(
-          (response)=>console.log('response from http: ' + response),
+          (response:string)=>{
+            console.log('response from http: ' + response);
+            this.response = response},
           (error)=>console.log('error: ' + error));
       }
     )
