1fe3e9d62391a52a662aa58f51176fbb516adc31
wangzixi-diablo <cle.ee@163.com>
2021-06-22 16:23:57 +0800

    add difference of BehaviorSubject and normal observable

--- src/app/rxjs/behaviorSubject/behavior-subject.component.ts
+++ src/app/rxjs/behaviorSubject/behavior-subject.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { of, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-behavior-subject',
@@ -8,6 +9,10 @@ import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 export class BehaviorSubjectComponent implements OnInit {
 
   ngOnInit(): void {
+
+    const jerry = of([1,2,3]);
+    
+    jerry.subscribe(console.log);
     const subject = new BehaviorSubject(123);
 
     // two new subscribers will get initial value => output: 123, 123
@@ -25,5 +30,25 @@ export class BehaviorSubjectComponent implements OnInit {
     // all three subscribers will get new value => output: 789, 789, 789
     console.log('subject emits 789!');
     subject.next(789);
+
+    console.log('latest value: ', subject.getValue());
+
+    const builtOE$ = subject.asObservable();
+
+    builtOE$.subscribe((data) => console.log('sub 4: ', data));
+
+    // builtOE$.next(); // no next method defined on normal Observable!
+
+    let normalSubject = new Subject(); 
+
+    normalSubject.next("b");
+
+    // 普通的 Subject, 下一行订阅语句，拿不到 b
+    normalSubject.subscribe(value => {
+      console.log("Subscription got", value); // Subscription wont get anything at this point
+    });
+
+    normalSubject.next("c"); // Subscription got c
+    normalSubject.next("d"); // Subscription got d
   }
 }
--- src/app/rxjs/rxjs.module.ts
+++ src/app/rxjs/rxjs.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CombineLatestComponent } from './combine-latest/combine-latest.component';
 import { SwitchMapComponent } from './switchMap/switchMap.component';
-import { BehaviorSubjectComponent } from './actionSubject/behavior-subject.component';
+import { BehaviorSubjectComponent } from './BehaviorSubject/behavior-subject.component';
 
 @NgModule({
   declarations: [CombineLatestComponent, SwitchMapComponent,
