0364fd8ebb769a81ba51487e31bf6adcd4e749f3
wangzixi-diablo <cle.ee@163.com>
2021-06-22 16:41:45 +0800

    switchMap compares with mergeMap

--- src/app/rxjs/mergeMap/merge-map.component.ts
+++ src/app/rxjs/mergeMap/merge-map.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { of, Subject, fromEvent } from 'rxjs';
-import { delay, mergeMap } from 'rxjs/operators';
-
+import { delay, mergeMap, switchMap } from 'rxjs/operators';
 
 interface Point {
   x: number;
@@ -10,19 +9,20 @@ interface Point {
   timestamp: number;
 }
 
+const saveLocation = (location:Point) => {
+  return of(location).pipe(delay(500));
+};
+
+const click$ = fromEvent(document, 'click');
+
 @Component({
   selector: 'app-merge-map',
   templateUrl: './merge-map.component.html'
 })
 export class MergeMapComponent implements OnInit {
 
-  ngOnInit(): void {
-    const saveLocation = (location:Point) => {
-      return of(location).pipe(delay(500));
-    };
+  setupMergeMap = () =>{
     // streams
-    const click$ = fromEvent(document, 'click');
-    
     click$
       .pipe(
         mergeMap((e: MouseEvent) => {
@@ -37,5 +37,26 @@ export class MergeMapComponent implements OnInit {
       // 不管我点得多快，每一次点击都会被记录下来
       .subscribe(r => console.log('Saved!', r.x, 
       r.y, r.timestamp));
+  };
+
+  setupSwitchMap = () => {
+    click$
+      .pipe(
+        switchMap((e: MouseEvent) => {
+          return saveLocation({
+            x: e.clientX,
+            y: e.clientY,
+            timestamp: Date.now()
+          });
+        })
+      )
+      // Saved! {x: 98, y: 170, ...}
+      // 点击速度变快时，只有一次点击会被记录下来
+      .subscribe(r => console.log('Switch Map Saved!', r.x, 
+      r.y, r.timestamp));
+  }
+  ngOnInit(): void {
+    // this.setupMergeMap();
+    this.setupSwitchMap();
   }
 }
