e6ade1e38039b081946b7d38ba9deaa0c99871fb
wangzixi-diablo <cle.ee@163.com>
2021-07-22 14:41:04 +0800

    only highlight when mouse enter

--- src/app/browserInjectionDemo/highLightDirective/highlight.directive.ts
+++ src/app/browserInjectionDemo/highLightDirective/highlight.directive.ts
@@ -1,10 +1,22 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
-})
-export class HighlightDirective {
-    constructor(el: ElementRef) {
-       el.nativeElement.style.backgroundColor = 'yellow';
+    selector: '[appHighlight]'
+  })
+  export class HighlightDirective {
+  
+    constructor(private el: ElementRef) { }
+  
+    @HostListener('mouseenter') onMouseEnter() {
+      this.highlight('yellow');
     }
-}
+  
+    @HostListener('mouseleave') onMouseLeave() {
+      this.highlight('');
+    }
+  
+    private highlight(color: string) {
+      this.el.nativeElement.style.backgroundColor = color;
+    }
+  
+  }
